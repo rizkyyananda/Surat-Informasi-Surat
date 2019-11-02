61a565e45507d97568c207a259d1a00e0e928abf
Rizky Yananda <yanandarizky@gmail.com>
2019-11-02 08:18:53 +0700

    add file

---
+++ app/Disposisi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Disposisi extends Model
+{
+    protected $table = 'disposisi';
+    protected $fillable = ['id_disposisi','disposisi'];
+
+}
